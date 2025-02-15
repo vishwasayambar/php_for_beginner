1adb4b337ebda253f6983e1f656d6bf6df358d9e
Vishwa <vishwajitsayambar92@gmail.com>
2025-02-15 19:26:26 +0530

    Config: Created Config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* We can use return direct in plane files also not only in function
+Also this config files is not specific to database it can be for any service we need dynamic so we have to a key to each config like database for Database config
+
+
+*/
+return [
+
+    'database' => [
+        'host' => '127.0.0.1',
+        'port' => '3306',
+        'dbname' => 'learning',
+        'charset' => 'utf8mb4'
+    ],
+
+];
